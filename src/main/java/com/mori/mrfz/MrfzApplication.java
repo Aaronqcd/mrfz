5960664ecebf006971fc399505b0f8ea222ee217
chuandong.qin <chuandong.qin@heweisoft.com>
2021-11-25 22:37:04 +0800

    提交spring boot初始代码

---
+++ src/main/java/com/mori/mrfz/MrfzApplication.java
@@ -0,0 +1,13 @@
+package com.mori.mrfz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrfzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrfzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mori/mrfz/MrfzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mori.mrfz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrfzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
