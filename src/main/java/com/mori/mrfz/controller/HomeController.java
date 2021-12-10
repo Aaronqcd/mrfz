d2dc3dfdb9131f9c9e80260ad8f5b7d9b4ce7d54
chuandong.qin <chuandong.qin@heweisoft.com>
2021-12-10 22:06:04 +0800

    1.完成首页 2.完成首页与魔窟界面的跳转功能

--- src/main/java/com/mori/mrfz/controller/HomeController.java
+++ src/main/java/com/mori/mrfz/controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = {"/home"})
-    public String index() {
-        return "home1";
+    @RequestMapping(value = {"/enemyIndex"})
+    public String enemyIndex() {
+        return "enemy";
     }
     @RequestMapping(value = {"/welcome"})
     public String welcome() {
--- src/main/java/com/mori/mrfz/controller/IndexController.java
+++ src/main/java/com/mori/mrfz/controller/IndexController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping(value = {"/index"})
     public String index() {
-        return "home1";
+        return "index";
     }
 }
