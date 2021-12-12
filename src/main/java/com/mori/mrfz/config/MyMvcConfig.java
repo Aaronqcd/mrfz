4c8b5243e0228226b5839571195e07315175aafa
chuandong.qin <chuandong.qin@heweisoft.com>
2021-12-12 21:19:31 +0800

    1.删除无用的注释

--- src/main/java/com/mori/mrfz/config/MyMvcConfig.java
+++ src/main/java/com/mori/mrfz/config/MyMvcConfig.java
@@ -3,11 +3,9 @@ package com.mori.mrfz.config;
 import com.mori.mrfz.interceptor.LoginHandlerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Locale;
@@ -22,19 +20,6 @@ public class MyMvcConfig implements WebMvcConfigurer {
                         , "/enemy/findAllHero", "/welcome", "/enemyIndex", "/css/**"
                         , "/js/**", "/images/**", "/fonts/**", "/lib/**");
     }
-
-    // 配置视图跳转
-    /*public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/index.html").setViewName("index");
-        registry.addViewController("/main.html").setViewName("dashboard");
-    }*/
-
-    // 将本地化解析器配置到容器中
-    /*@Bean
-    public LocaleResolver localeResolver() {
-        return new MyLocaleResolver();
-    }*/
 
     // 将视图解析器配置到容器中
     @Bean
